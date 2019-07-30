4d0404268f6a2fba8aa3d48d6d4c1e29b082215e
Oksana <ksenia.barshchyk@yandex.ru>
2019-07-30 23:36:53 +0300

    Added new file.

---
+++ string/palindrome_string.py
@@ -0,0 +1,5 @@
+"""
+How to check if String is Palindrome.
+Another easy coding question based upon String, I am sure you must have done this numerous time. Your program should return true if String is a Palindrome, otherwise false. For example, if the input is "radar", the output should be true, if the input is "madam" output will be true, and if the input is "Python" output should be false.
+
+"""
